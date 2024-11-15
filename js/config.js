4d0e9931dd865930e62781067b4e49bb8769c45a
lamncn <ncnlam@gmail.com>
2024-11-15 14:57:56 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 {
-    "api": "https://www.tienvuc.live",
+    "api": "https://www.viptruyenfull.live",
     "customdns": "on",
     "customdnsurl": "https://dns.google/dns-query",
     "key": "dnt",
