526cc4ecc81bbc59c1395175164ff2355c93afda
lamncn <ncnlam@gmail.com>
2025-01-19 13:38:17 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 {
-    "api": "https://www.truyentuongtac.live",
-    "customdns": "on",
+    "api": "https://www.namtaynguyen.live",
+    "customdns": "off",
     "customdnsurl": "https://1.1.1.1/dns-query",
     "key": "dnt",
     "value": "1"
